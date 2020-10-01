32bbaa73ff4f65ba84d51ab9cc01ce9c4d120810
Daeseong Shin <esc990720@gmail.com>
2020-10-01 09:54:57 +0000

    Apply typescript to express server

---
+++ ROKTION-server/src/server.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response, NextFunction} from "express";
+
+class App {
+    public application : express.Application;
+
+    constructor() {
+        this.application = express();
+    }
+}
+
+const app = new App().application;
+const PORT = 5000;
+
+app.get('/data', (req:Request, res:Response, next:NextFunction) => {
+    const data = {
+        lastname : "dl",
+        firstname : "wlrma"
+    };
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
