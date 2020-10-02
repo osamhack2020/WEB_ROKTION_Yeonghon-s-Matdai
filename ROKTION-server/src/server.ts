9d1dd50ec469cf4665edc939fc7ce2164a8b5b78
Daeseong Shin <esc990720@gmail.com>
2020-10-02 02:06:36 +0000

    Add status 404 on server

--- ROKTION-server/src/server.ts
+++ ROKTION-server/src/server.ts
@@ -5,10 +5,16 @@ import dataRouter from './dataRouter';
 const app = express();
 const PORT = 5000;
 
-// middleware
+// middlewares
 app.use(logger('dev'));
 
+// routers
 app.use(dataRouter);
+
+// 404 Not Found
+app.use((req: Request, res: Response) => {
+    res.status(404).send('NOT FOUND');
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
