987298b23aa7f8838eace78edc0ad0720d2f1e1c
Merseong <esc990720@korea.ac.kr>
2020-10-10 07:07:37 +0000

    Add logoff api

--- ROKTION-server/src/routers/user.ts
+++ ROKTION-server/src/routers/user.ts
@@ -1,8 +1,25 @@
 import express, { Request, Response, NextFunction } from "express";
-import { User, UserModel } from "../schemas/user";
+import { UserModel } from "../schemas/user";
 import crypto from 'crypto';
 
 const router = express.Router();
+
+// 로그오프
+router.get('/logoff', (req: Request, res: Response) => {
+    if (req.session?.dbId) {
+        req.session.destroy(err => {
+            if (err) {
+                // 로그오프 실패
+                console.log(err);
+            } else {
+                // 정상 로그오프
+                res.redirect('/');
+            }
+        })
+    } else {
+        res.redirect('/');
+    }
+})
 
 router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
     const toSend = {
@@ -10,9 +27,9 @@ router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
     };
     getTagId(req.params.id)
     .then(n => {
-        UserModel.find({ tagId: n })
-        .then((users) => {
-            toSend.data = users[0];
+        UserModel.findOne({ tagId: n }, {_id: 0, passwd: 0, passwdSalt: 0})
+        .then((user) => {
+            toSend.data = user!;
             res.json(toSend);
         })
         .catch((err) => {
@@ -40,7 +57,7 @@ router.post('/login', (req: Request, res: Response) => {
             crypto.pbkdf2(req.params.pw, usr.passwdSalt, 1231, 64, 'sha512', (err, key) => {
                 if (key.toString('base64') === usr.passwd) {
                     // 맞으면 세션 생성, _id와 tagId를 저장해둔다.
-                    req.session!.id = usr._id;
+                    req.session!.dbId = usr._id;
                     req.session!.tagId = usr.tagId;
                     res.status(200).end();
                 } else {
@@ -88,12 +105,9 @@ router.put('/:id', (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body);
     getTagId(req.params.id)
     .then(n => {
-        UserModel.update({ tagId: n }, {...req.body})
-        .then(() => res.status(200).end())
-        .catch(e => {
-            throw new Error(e);
-        })
+        return UserModel.update({ tagId: n }, {...req.body})
     })
+    .then(() => res.status(200).end())
     .catch(e => {
         res.status(400).end();
     });    
