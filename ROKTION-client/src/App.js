39f0852d183804b93b366688bfcd9b99de28a772
DongHu Kim <kty2396@gmail.com>
2020-10-14 09:47:34 +0000

    fixed tag filtering

--- ROKTION-client/src/App.js
+++ ROKTION-client/src/App.js
@@ -19,6 +19,21 @@ class App extends Component {
                 name:null,
             },
             selectedDocumentId: -1,
+            tagsId:12,
+            tags:[
+                { name: "진행중", id: 0, color: "#016936" },
+                { name: "예정됨", id: 1, color: "#A0A0A0" },
+                { name: "완료됨", id: 2, color: "#EE82EE" },
+                { name: "인수인계", id: 3, color: "#008080" },
+                { name: "문서", id: 4, color: "#A52A2A" },
+                { name: "상황병", id: 5, color: "#FF1493" },
+                { name: "중대행정병", id: 6, color: "#B03060" },
+                { name: "지원과", id: 7, color: "#B413EC" },
+                { name: "간부", id: 8, color: "#32CD32" },
+                { name: "징계위원회", id: 9, color: "#0E6EB8" },
+                { name: "분대장", id: 10, color: "#FE9A76" },
+                { name: "임시", id: 11, color: "#000000"},
+            ],
             documents:[
                 {
                     title: "내가 진행중인 업무",
@@ -35,6 +50,7 @@ class App extends Component {
                                       <DocumentPageContent content="POGPOGPOGPOGPOG"/>,
                                       <DocumentPageContent content="POGPOGPOGPOGPOGPOG"/>,
                                       <DocumentPageContent content="POGPOGPOGPOGPOGPOGPOG"/>,],
+                    tags:[0,4,5],
                 },
                 {
                     title: "예정된 업무",
@@ -44,6 +60,7 @@ class App extends Component {
                     id: 2,
                     onClick: ()=>{this.setState({selectedDocumentId:2});},
                     documentContent: [<DocumentPageContent content="That's WeirdChamp bro WeirdChamp"/>],
+                    tags:[1,3,6],
                 },
                 {
                     title: "종료된 업무",
@@ -53,14 +70,10 @@ class App extends Component {
                     id: 3,
                     onClick: ()=>{this.setState({selectedDocumentId:3});},
                     documentContent: [<DocumentPageContent content="S OMEGALUL BAD"/>],
+                    tags:[2,7,8],
                 },
-                { 
-                    title:"PlaceHolder", 
-                    description:"Holding place :/", 
-                    alert:0, 
-                    id:4, 
-                    onClick:()=>{console.log("POGGERS")}
-                },
+                { title:"PlaceHolder", description:"Holding place :/", alert:0, id:4, onClick:()=>{console.log("POGGERS")},tags:[11],},
+                { title:"PlaceHolder", description:"Holding place :/", alert:0, id:4, onClick:()=>{console.log("POGGERS")},tags:[11],},
             ]
           };
     }
@@ -146,6 +159,7 @@ class App extends Component {
                     description: '',
                     alert: 10,
                     id: 5,
+                    tags:[11],
                     onClick: () => {this.setState({selectedDocumentId:5})},
                     documentContent: [<DocumentPageContent content={docInfo.title}/>],
                 }); 
@@ -154,6 +168,19 @@ class App extends Component {
                 console.error(e);
             })
         }
+    }
+
+    addNewTag = (name, color) => {
+        const tags = this.state.tags;
+        const tagsId = this.state.tagsId;
+        this.setState({
+            tags: tags.concat({
+                name: name,
+                id: this.state.tagsId,
+                color: color,
+            }),
+            tagsId: tagsId+1,
+        })
     }
 
     render() {
@@ -177,8 +204,9 @@ class App extends Component {
                         <MainMenuLayout
                         handleLogout={this.onLogout}
                         userInfo={this.state.userInfo}
+                        addNewTag={this.addNewTag}
                         documents={documents}
-                        tags={[]}/>
+                        tags={this.state.tags}/>
                     </Transition>
             );
         }
