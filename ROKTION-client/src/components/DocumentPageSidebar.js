df29542be5a34b65c879eb627f91de543afeaf99
DongHu Kim <kty2396@gmail.com>
2020-10-20 13:38:13 +0000

    updated sidebar layout

--- ROKTION-client/src/components/DocumentPageSidebar.js
+++ ROKTION-client/src/components/DocumentPageSidebar.js
@@ -27,40 +27,57 @@ const DocumentPageSidebar = (props) => {
   const [activeIndex, setActiveIndex] = React.useState(-1)
   const documents = props.documents
 
-  let list = documents.map(
-    data => (
-      <Menu.Item
-        key={data.id}
-        style={{paddingTop:"0px", paddingBottom:"0px", marginTop:"15px"}}
-        onClick={()=>{data.onClick(); setVisible(false);}}>
-        <Grid>
-          <Grid.Row
-            columns='equal'
-            verticalAlign='middle'
-            style={{paddingTop:'5px', paddingBottom:"5px", minHeight:"32px"}}>
-            <Grid.Column
-              textAlign='left'
-              style={{lineHeight:'17px', paddingRight:'9px'}}>
-                {data.title}
-            </Grid.Column>
-            <Grid.Column
-              width={1}
-              textAlign='center'
-              style={{padding:'0px 49px 0px 0px'}}>
-              {data.alert !== 0 && <Label size='mini' color='red'> {data.alert} </Label>}   
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Menu.Item>
-
-      
-      
-    )
-  );
-
   let totalAlerts = 0;
   documents.forEach(data => (totalAlerts += data.alert));
 
+  const mainTagList = ['진행','예정','완료','문서','중요'].map(
+    (tag, idx) => (
+    <>
+      <Accordion.Title
+        active={activeIndex === idx}
+        index = {idx}
+        onClick = {()=>{setActiveIndex(activeIndex === idx ? -1 : idx);}}>
+        <Icon name='dropdown'/>
+        {tag}
+      </Accordion.Title>
+      <Accordion.Content
+        active={activeIndex===idx}
+        style={{paddingTop:'0px', paddingBottom:'0px'}}>
+      <Menu size='massive' fluid vertical secondary>
+        {documents.map(
+          data => (
+            data.tags.has(idx) &&
+            <Menu.Item
+              key={data.id}
+              style={{paddingTop:"0px", paddingBottom:"0px", marginTop:"15px"}}
+              onClick={()=>{data.onClick(); setVisible(false);}}>
+              <Grid>
+                <Grid.Row
+                  columns='equal'
+                  verticalAlign='middle'
+                  style={{paddingTop:'5px', paddingBottom:"5px", minHeight:"32px"}}>
+                  <Grid.Column
+                    textAlign='left'
+                    style={{lineHeight:'17px', paddingRight:'9px'}}>
+                      {data.title}
+                  </Grid.Column>
+                  <Grid.Column
+                    width={1}
+                    textAlign='center'
+                    style={{padding:'0px 49px 0px 0px'}}>
+                    {data.alert !== 0 && <Label size='mini' color='red'> {data.alert} </Label>}   
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Menu.Item>
+          )
+        )
+        }
+      </Menu>
+      </Accordion.Content>
+    </>
+    )
+  )
   return (
     <>
       <Sidebar
@@ -133,46 +150,7 @@ const DocumentPageSidebar = (props) => {
               inverted
               fluid
               style={{paddingLeft:'15px'}}>
-                <Accordion.Title
-                  active={activeIndex === 0}
-                  index = {0}
-                  onClick = {()=>{setActiveIndex(activeIndex === 0 ? -1 : 0);}}>
-                  <Icon name='dropdown'/>
-                  진행중
-                </Accordion.Title>
-                <Accordion.Content
-                  active={activeIndex===0}>
-                <Menu size='massive' fluid vertical secondary>
-                  {list}
-                </Menu>
-                </Accordion.Content>
-
-                <Accordion.Title
-                  active={activeIndex === 1}
-                  index = {1}
-                  onClick = {()=>{setActiveIndex(activeIndex === 1 ? -1 : 1);}}>
-                  <Icon name='dropdown'/>
-                  예정됨
-                </Accordion.Title>
-                <Accordion.Content
-                  active={activeIndex===1}>
-                <Menu size='massive' fluid vertical secondary>
-                  {list}
-                </Menu>
-                </Accordion.Content>
-
-                <Accordion.Title
-                  active={activeIndex === 2}
-                  index = {2}
-                  onClick = {()=>{setActiveIndex(activeIndex === 2 ? -1 : 2);}}>
-                  <Icon name='dropdown'/>
-                  완료됨
-                </Accordion.Title>
-                <Accordion.Content>
-                <Menu size='massive' fluid vertical secondary>
-                  {list}
-                </Menu>
-                </Accordion.Content>
+                {mainTagList}
             </Accordion>
           </Grid.Row>
         </Grid>
